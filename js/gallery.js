2fac57a23fd97f7d6c11bc7397e5abbfd798b619
SlavaGrach <grachv77@gmail.com>
2021-04-18 19:52:00 +0300

    finaly commit

--- js/gallery.js
+++ js/gallery.js
@@ -5,6 +5,8 @@ const galleryModalContainer = document.querySelector('.js-lightbox');
 const lightBoxImage = document.querySelector('.lightbox__image');
 const modalCloseBut = document.querySelector('.lightbox__button');
 
+let curentIndex = 0;
+
 const makeGalleryImage = ({preview, original, description}) => {
     const galleryItemEl = document.createElement('li');
     galleryItemEl.classList.add('gallery__item');
@@ -31,27 +33,62 @@ galleryContainer.append(...elements);
 
 galleryContainer.addEventListener('click', onClic);
 
+galleryModalContainer.addEventListener('click', onModalCloseClick);
+
+window.addEventListener('keydown', onModalKeyPress);
+
+
 function onClic(evt) {
     if (evt.target.nodeName !== 'IMG') {
-        console.log(evt.target.nodeName);
         return;
     }
     evt.preventDefault();
     galleryModalContainer.classList.add('is-open');
-    lightBoxImage.src = evt.target.src;
+    lightBoxImage.src = evt.target.dataset.source;
+
+    curentIndex = galleryItems.findIndex((item, index, array) => {
+        return item.original === evt.target.dataset.source;
+    });
 
 };
 
-galleryModalContainer.addEventListener('click', onBtnClick);
+function onModalCloseClick(evt) {
 
-function onBtnClick(evt) {
-    if (evt.target.nodeName !== 'BUTTON') {
+    if ((evt.target.nodeName !== 'BUTTON') && (evt.target.nodeName !== 'IMG')) {
         return;
     }
 
     galleryModalContainer.classList.remove('is-open');
     lightBoxImage.src = '';
 }
+
+function onModalKeyPress(evt) {
+    
+    if ((evt.code !== 'Escape') && (evt.code !== 'ArrowLeft') && (evt.code !== 'ArrowRight')) {
+        return;
+    }
+
+    if (evt.code === 'Escape') {
+        galleryModalContainer.classList.remove('is-open');
+        lightBoxImage.src = '';
+    } else if (evt.code !== 'ArrowRight') {
+        curentIndex -= 1;
+        if (curentIndex < 0) {
+            curentIndex = galleryItems.length - 1;
+        }
+        lightBoxImage.src = galleryItems[curentIndex].original;
+    } else {
+        curentIndex += 1;
+        if (curentIndex > galleryItems.length - 1) {
+            curentIndex = 0;
+        }
+        lightBoxImage.src = galleryItems[curentIndex].original;
+    }
+}
+
+
+
+
 
 
 
